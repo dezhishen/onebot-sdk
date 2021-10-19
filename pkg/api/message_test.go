fd872e43616b281b502638d9f8a8d5d4c82a71b6
dezhiShen <1179551960@qq.com>
2021-10-19 23:01:24 +0800

    feat:增加测试

---
+++ pkg/api/message_test.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/dezhishen/onebot-sdk/pkg/model"
+)
+
+var userId int64 = 123456
+var groupId int64 = 123456
+
+func TestSendMsg(t *testing.T) {
+	msg := model.MsgForSend{
+		MessageType: "private",
+		UserID:      userId,
+		Message: []*model.MessageSegment{
+			{
+				Type: "text",
+				Data: &model.TextMessage{
+					Text: "测试消息",
+				},
+			},
+		},
+	}
+	got, err := SendMsg(&msg)
+	if err != nil {
+		t.Errorf("SendMsg() error = %v", err)
+		return
+	}
+	print(got)
+}
+
+func TestSendGroupMsg(t *testing.T) {
+	msg := model.GroupMsg{
+		GroupID: groupId,
+		Message: []*model.MessageSegment{
+			{
+				Type: "text",
+				Data: &model.TextMessage{
+					Text: "测试消息",
+				},
+			},
+		},
+	}
+	got, err := SendGroupMsg(&msg)
+	if err != nil {
+		t.Errorf("SendGroupMsg() error = %v", err)
+		return
+	}
+	print(got)
+}
+
+func TestSendPrivateMsg(t *testing.T) {
+	msg := model.PrivateMsg{
+		UserID: userId,
+		Message: []*model.MessageSegment{
+			{
+				Type: "text",
+				Data: &model.TextMessage{
+					Text: "测试消息",
+				},
+			},
+		},
+	}
+	got, err := SendPrivateMsg(&msg)
+	if err != nil {
+		t.Errorf("SendPrivate() error = %v", err)
+		return
+	}
+	print(got)
+}
