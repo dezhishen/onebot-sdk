150626419ff5cea7952480ec49a687659d5440b4
dezhiShen <1179551960@qq.com>
2021-11-27 10:36:26 +0800

    mod(ws):增加Websocket入口

--- pkg/event/message.go
+++ pkg/event/message.go
@@ -3,8 +3,6 @@ package event
 import (
 	"fmt"
 	"net/url"
-	"os"
-	"time"
 
 	"github.com/dezhishen/onebot-sdk/pkg/config"
 	"github.com/gorilla/websocket"
@@ -21,47 +19,16 @@ func StartWs() {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-	done := make(chan struct{})
-	interrupt := make(chan os.Signal, 1)
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				return
-			}
-			log.Printf("%s", message)
-		}
-	}()
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
 	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-		case <-interrupt:
-			log.Println("interrupt")
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
 			return
 		}
+		log.Printf("%s", message)
 	}
 }
 
-func ListenReciverPrivateMsg() {
+func ListenReciverPrivateMsg(func(data []byte) error) {
 
 }
