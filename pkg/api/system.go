b962047c95c07617cf614fa568597ed8f4cc9a0d
fuyiweiwow <32919129+fuyiweiwow@users.noreply.github.com>
2021-12-10 13:53:14 +0800

    refactor system api

--- pkg/api/system.go
+++ pkg/api/system.go
@@ -1,96 +1,49 @@
 package api
 
 import (
-	"bytes"
-	"encoding/json"
-	"io"
-	"net/http"
-
-	"github.com/dezhishen/onebot-sdk/pkg/config"
+	"github.com/dezhishen/onebot-sdk/pkg/cli"
 	"github.com/dezhishen/onebot-sdk/pkg/model"
 )
 
 func CleanCache() error {
-	_, err := http.Post(
-		config.GetHttpUrl()+"/clean_cache",
-		"application/json",
-		nil,
-	)
-	return err
+	return cli.Post("/clean_cache")
 }
 
 func SetRestart(delay int) error {
 	reqMap := make(map[string]interface{})
 	reqMap["delay"] = delay
-	requestBody, _ := json.Marshal(reqMap)
-	_, err := http.Post(
-		config.GetHttpUrl()+"/set_restart",
-		"application/json",
-		bytes.NewBuffer(requestBody),
-	)
-	return err
+
+	return cli.PostWithRequest("/set_restart", reqMap)
 }
 
 func GetVersionInfo() (map[string]interface{}, error) {
-	resp, err := http.Post(
-		config.GetHttpUrl()+"/get_version_info",
-		"application/json",
-		nil,
-	)
-	if err != nil {
+	var result model.MapInfoResult
+	if err := cli.PostForResult("/get_version_info", &result); err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	respBodyContent, _ := io.ReadAll(resp.Body)
-	var result model.MapInfoResult
-	json.Unmarshal(respBodyContent, &result)
 	return result.Data, nil
 }
 
 func GetStatus() (map[string]interface{}, error) {
-	resp, err := http.Post(
-		config.GetHttpUrl()+"/get_status",
-		"application/json",
-		nil,
-	)
-	if err != nil {
+	var result model.MapInfoResult
+	if err := cli.PostForResult("/get_status", &result); err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	respBodyContent, _ := io.ReadAll(resp.Body)
-	var result model.MapInfoResult
-	json.Unmarshal(respBodyContent, &result)
 	return result.Data, nil
 }
 
 func CanSendImage() (map[string]interface{}, error) {
-	resp, err := http.Post(
-		config.GetHttpUrl()+"/can_send_image",
-		"application/json",
-		nil,
-	)
-	if err != nil {
+	var result model.MapInfoResult
+	if err := cli.PostForResult("/can_send_image", &result); err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	respBodyContent, _ := io.ReadAll(resp.Body)
-	var result model.MapInfoResult
-	json.Unmarshal(respBodyContent, &result)
 	return result.Data, nil
 }
 
 func CanSendRecord() (map[string]interface{}, error) {
-	resp, err := http.Post(
-		config.GetHttpUrl()+"/can_send_record",
-		"application/json",
-		nil,
-	)
-	if err != nil {
+	var result model.MapInfoResult
+	if err := cli.PostForResult("/can_send_record", &result); err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	respBodyContent, _ := io.ReadAll(resp.Body)
-	var result model.MapInfoResult
-	json.Unmarshal(respBodyContent, &result)
 	return result.Data, nil
 }
