31e17a55e4c4f8d9bdd5392c6489a72315be46cc
dezhiShen <1179551960@qq.com>
2023-03-02 00:59:16 +0800

    fix:移除不必要的启动入口

--- cmd/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/dezhishen/onebot-sdk/pkg/model"
-
-func main() {
-	jsonStr := "{\"type\":\"node\",\"data\":{\"user_id\":\"10001000\",\"nickname\":\"某人\",\"content\":[{\"type\":\"face\",\"data\":{\"id\":\"123\"}},{\"type\":\"text\",\"data\":{\"text\":\"哈喽～\"}}]}}"
-	model.GenMessageElement(jsonStr, "./pkg/model")
-}
