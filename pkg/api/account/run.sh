83f1daa353f340a041b9e1826231d14296c4df83
dezhiShen <1179551960@qq.com>
2023-03-01 19:39:50 +0800

    mod: 修改proto的位置

--- pkg/api/account/run.sh
+++ pkg/api/account/run.sh
@@ -1,5 +1,5 @@
-MYDIR=C:/github/onebot-sdk/pkg/proto
-INCLUDE=C:/Users/11795/go/include
+MYDIR=C:/github/onebot-sdk/pkg/api/account
+INCLUDE=$GOPATH/include
 for file in ./*
 do
     if [ "${file##*.}"x = "proto"x ]; then
@@ -12,7 +12,7 @@ do
 		# }"
         # echo "$file" 
         #echo "protoc --proto_path=$INCLUDE --proto_path=$MYDIR --go_out=plugins=grpc:\model\  $MYDIR/$file"
-        protoc --proto_path=$INCLUDE --proto_path=$MYDIR --go_out=plugins=grpc:../model  $MYDIR/$file
+        protoc --proto_path=$INCLUDE --proto_path=C:/github --proto_path=C:/github/onebot-sdk/pkg/proto --proto_path=$MYDIR --go_out=plugins=grpc:../proto  $MYDIR/$file
         #echo "$x $file $file = 2;"
     fi
 done   
---
+++ pkg/model/run.sh
@@ -0,0 +1,18 @@
+MYDIR=C:/github/onebot-sdk/pkg/model
+INCLUDE=C:/Users/11795/go/include
+for file in ./*
+do
+    if [ "${file##*.}"x = "proto"x ]; then
+        # a="${file/message_element_/}"
+        # b="${a/\.\//}"
+        # c="${b/.proto/}"
+        # echo "case \"$c\":
+		# result.Data = &MessageSegmentGRPC_MessageElement${c^}{
+		# 	MessageElement${c^}: msg.Data.(*MessageElement${c^}).ToGRPC(),
+		# }"
+        # echo "$file" 
+        #echo "protoc --proto_path=$INCLUDE --proto_path=$MYDIR --go_out=plugins=grpc:\model\  $MYDIR/$file"
+        protoc --proto_path=$INCLUDE --proto_path=$MYDIR --go_out=plugins=grpc:../model  $MYDIR/$file
+        #echo "$x $file $file = 2;"
+    fi
+done   
